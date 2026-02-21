6665e5aa6bb9f6c5cf8eeea5ad17ea5f68e7e43b
patricksullivan123411223344 <patrick.sullivan@uri.edu>
2026-02-21 11:38:29 -0500

    merge. finished dependency setup and starting main App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import React from 'react'
+import { heroui } from "@heroui/react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const HeroSection = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Find your perfect home...</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Card className="py-4">
+      <CardBody className="heroCardBody">
+        <div className="heroCardInterior">
+          <h1 className="heroText">Find your landlord rating...</h1>
+          <p className="heroInfo">Do a deep search on your landlords lease history</p>
+        </div>
+      </CardBody>
+    </Card>
   )
 }
 
